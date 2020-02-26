0a5d108e04f1f312eee0a460dd0cdd7421215271
Georgi Ganchev <googs.bs@gmail.com>
2020-02-26 16:43:27 +0000

    emit 'started' callback to the config

--- lib/cuetip/models/job.rb
+++ lib/cuetip/models/job.rb
@@ -67,6 +67,8 @@ module Cuetip
         # Initialize a new instance of the job we wish to execute
         job_klass = class_name.constantize.new(self)
 
+        Cuetip.config.emit(:started, self, job_klass)
+
         # If the job has expired, we should not be executing this so we'll just
         # remove it from the queue and mark it as expired.
         if expired?
